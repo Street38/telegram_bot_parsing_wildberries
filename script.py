8b725bff93b9b544544d0c3c6c80ac98b81dd6dd
Igor <irstreet08@gmail.com>
2022-05-07 13:16:56 +0800

    get_price_07.05.22

---
+++ script.py
@@ -0,0 +1,35 @@
+import telebot
+from token_bot import TOKEN
+import requests
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+HEADERS = {
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0'
+}
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])                                        # Приветсвие и ожидание получения ссылки на товар
+def get_url(message):
+    sent = bot.reply_to(message, 'Привет, отправь ссылку для отслеживания')
+    bot.register_next_step_handler(sent, get_price)
+
+def get_price(message):                                                          # Получаем HTML по ссылке
+    message_save = message.text
+    if 'http' in message_save:
+        browser = webdriver.Firefox()
+        browser.get(message_save)
+        time.sleep(5)
+        block = browser.find_element_by_class_name('same-part-kt__price-block')
+        block_arguments = (block.text).split('₽')
+        price = int((block_arguments[0]).replace(' ', ''))
+        bot.send_message(message.chat.id, f'Цена: {price} р.')
+        browser.quit()
+    else:
+        bot.send_message(message.chat.id, 'Не верная ссылка, для повтора нажмите /start')
+
+
+bot.polling()
